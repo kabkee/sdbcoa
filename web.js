9311c9f5024587fa3c67f1264dd538d3aaa71abb
Kabkee Moon <kabkee@live.com>
2013-03-09 02:07:22 +0900

    little comment

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('What\'s up? This page is gonna be a great web app by Kabkee Moon :D<BR> Hold on a second.<br>You may visit <a href="https://googledrive.com/host/0B70xA4jw5f9benN1RzlCMkFMckE/index.html">here</a>');
 });
 
 var port = process.env.PORT || 5000;
