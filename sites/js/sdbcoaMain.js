618401bbeb142403a1f77389b92d98f093573f89
Kabkee Moon <kabkee@live.com>
2013-05-09 21:57:25 +0900

    lot request list added not yet completed

--- sites/js/sdbcoaMain.js
+++ sites/js/sdbcoaMain.js
@@ -1,106 +1,157 @@
+var $window = $(window);
+var $alert = $('.alert');
+var $divAlert = $('#divAlert');
+
+var $loginContainer = $('#loginContainer');
+var $mngContainer = $('#mngContainer');
+var $main = $('#main');
+var $dataMng = $('#dataMng');
+var $coaMng = $('#coaMng');
+var $Gen = $('#Gen');
+var $lotResult = $('#lotResult');
+var $lotRequest = $('#lotRequest');
+var $aboutUs = $('#aboutUs');
+
+var $aHome = $('#aHome');
+var $aDataMng = $('#aDataMng');
+var $aCoaMng = $('#aCoaMng');
+var $aLotRequest = $('#aLotRequest');
+var $aAboutUs = $('#aAboutUs');
+
+var menuBtnList = [$main, $dataMng, $coaMng, $lotRequest, $aboutUs];
+
+var $inputCatNo = $('#inputCatNo');
+var $inputLotNo = $('#inputLotNo');
+var $inputMFGDate = $('#inputMFGDate');
+var $inputExpDate = $('#inputExpDate');
+
+// Lot MNG Search click.
+var $btnSearchLot = $('#btnSearchLot');
+
+var $btnStore = $('#btnStore');
+var $previewDiv = $('#previewDiv');
+var $lotRequestQueNew = $('#lotRequestQueNew');
+var $lotRequestQue = $('#lotRequestQue');
+
 var app = {
-    prevSrvStatus: false,
     srvStatus: false,
     reLive : false,
-
-    getPrevStatus: function(){
-        return this.prevSrvStatus;
-    },
     getStatus: function(){
         return this.srvStatus;
     },
-    setPrevStatus: function(status){
-        this.prevSrvStatus = status;
-    },
     setStatus: function(status){
+        this.srvStatus = status;
+    },
+    getReLive: function(){
+        return this.srvStatus;
+    },
+    setReLive: function(status){
         this.srvStatus = status;
     }
 };
 var appConnect = {
+    GetLotRequest: function(){
 
+    },
     GetStatus: function(){
         var result;
         $.get('/status', function(data){
             if(data.status == true){
-                if(app.srvStatus==false){
-                    app.reLive = true;
+                if(app.getStatus()==false){
+                    app.setReLive(true);
                 }else{
-                    app.reLive = false;
+                    app.setReLive(false);
                 }
                 result = true;
             }else if(data.status == false){
                 result = false;
             }
-            app.prevSrvStatus = app.srvStatus;
-            app.srvStatus = data.status;
+            app.setStatus(data.status);
             return result;
         });
     }
 };
-var appAlert ={
 
-//    잠시 서버가 중단 되었습니다. 장기간 지속될 경우, 담당자에게 연락하세요. alert-error
-//    서버가 다시 살아났습니다~ :) alert-success
-};
+var testLotRequest = { data: [ { lot : "c01234567", expDate: "2013-10-01", mfgDate: "2011-01-01", new: false},
+    { lot : "c01234568", expDate: "2013-10-02", mfgDate: "2011-01-02", new: false},
+    { lot : "c01234569", expDate: "2013-10-03", mfgDate: "2011-01-03", new: true},
+    { lot : "c01234570", expDate: "2013-10-04", mfgDate: "2011-01-04", new: false},
+    { lot : "c01234571", expDate: "2013-10-05", mfgDate: "2011-01-05", new: true} ] };
 
-$(document).ready(function() {
-    var $window = $(window);
-    var $alert = $('.alert');
-    var $divAlert = $('#divAlert');
-    function showAlertFnc(statusOn){
-        // WHAT IF RE-LIVE IS TRUE;
-        if(statusOn == false){
-            var $divAlert_span = $('#divAlert_span');
-            $divAlert_span.css({"marginLeft": 20+"px", "top": $divAlert.height() + "px"})
-                .text("잠시 서버가 중단 되었습니다. 장기간 지속될 경우, 담당자에게 연락하세요.");
-            $divAlert.addClass("alert-error").show("fast");
-        }else{
-            $divAlert.hide();
-        }
-    }
+function makeListOfLotRequest(data){
+    if(data){
+        for(var i=0; i<data.data.length; i++){
+            var eachItem = data.data[i];
+            if(eachItem.new == true){
+                var listItem = "<ol class ='selectable'>";
+                listItem = "<li class='ui-widget-content'>" +
+                    "<input type='text' value="+  + ">eachItem.lot</input>" +
+                    "<input type='text' value="+  + ">eachItem.expDate</input>" +
+                    "<input type='text' value="+  + ">eachItem.mfgDate</input>" +
+                    "</li>";
+                listItem = "</ol>";
+                $lotRequestQueNew.html(listItem);
+                $lotRequestQueNew.selectable;
+            }else{
 
-    $window.scroll(function() {
-        var position = $window.scrollTop();
-        $divAlert.css({"top": ( $divAlert.height()) + "px"});
-    });
-
-    var $loginContainer = $('#loginContainer');
-    var $mngContainer = $('#mngContainer');
-    var $main = $('#main');
-    var $dataMng = $('#dataMng');
-    var $coaMng = $('#coaMng');
-    var $Gen = $('#Gen');
-    var $lotResult = $('#lotResult');
-    var $lotRequest = $('#lotRequest');
-    var $aboutUs = $('#aboutUs');
-
-    var $aHome = $('#aHome');
-    var $aDataMng = $('#aDataMng');
-    var $aCoaMng = $('#aCoaMng');
-    var $aLotRequest = $('#aLotRequest');
-    var $aAboutUs = $('#aAboutUs');
-
-    var menuBtnList = [$main, $dataMng, $coaMng, $lotRequest, $aboutUs];
-    function showMenu(MenuNameWannaShow){
-        MenuNameWannaShow.show("fast");
-        for(var i=0; i<menuBtnList.length; i++){
-            if(MenuNameWannaShow != menuBtnList[i]){
-                menuBtnList[i].hide("fast");
             }
         }
+    }else{
+        $lotRequestQueNew.html("<h1>자료가 없습니다.</h1>");
+        $lotRequestQue.html("<h1>자료가 없습니다.</h1>");
     }
+}
+
+
+function showAlertFnc(statusOn){
+    if(statusOn == false){
+        var $divAlert_span = $('#divAlert_span');
+        $divAlert_span.css({"marginLeft": 20+"px", "top": $divAlert.height() + "px"})
+            .text("잠시 서버가 중단 되었습니다. 장기간 지속될 경우, 담당자에게 연락하세요.");
+        $divAlert.addClass("alert-error").show("fast");
+    }else{
+        // WHAT IF RE-LIVE IS TRUE;
+        if(app.getReLive() == true){
+            $divAlert_span.css({"marginLeft": 20+"px", "top": $divAlert.height() + "px"})
+                .text("서버가 다시 살아났습니다~  :) 이힛.");
+            $divAlert.addClass("alert-info").show("fast");
+        }
+        setTimeout(function(){
+            $divAlert.hide();
+        },3000);
+    }
+}
+function showMenu(MenuNameWannaShow){
+    MenuNameWannaShow.show("fast");
+    for(var i=0; i<menuBtnList.length; i++){
+        if(MenuNameWannaShow != menuBtnList[i]){
+            menuBtnList[i].hide("fast");
+        }
+    }
+}
+function firstLoading(){
     showAlertFnc(appConnect.GetStatus());
 //    showAlertFnc(false);
 
     //Gen Hide - rolling loading image
     $Gen.hide();
+
     //loginContainer Hide
     $loginContainer.hide();
-    //alert Hide
-//    $alert.hide();
+
     // first show menu is $lotRequest
     showMenu($lotRequest);
 
+    makeListOfLotRequest(testLotRequest);
+}
+
+
+$(document).ready(function() {
+
+    firstLoading();
+
+
+    // Clicking Menu, change screen
     $aHome.click( function(){
         showMenu($main);
     });
@@ -117,14 +168,6 @@ $(document).ready(function() {
         showMenu($aboutUs);
     });
 
-
-    var $inputCatNo = $('#inputCatNo');
-    var $inputLotNo = $('#inputLotNo');
-    var $inputMFGDate = $('#inputMFGDate');
-    var $inputExpDate = $('#inputExpDate');
-
-    // Lot MNG Search click.
-    var $btnSearchLot = $('#btnSearchLot');
     $btnSearchLot.on('click', function(e){
         $Gen.show();
         $.post('/lots/search',{catNo: $inputCatNo.val(), lotNo: $inputLotNo.val(), mfgDate: $inputMFGDate.val()
@@ -136,8 +179,7 @@ $(document).ready(function() {
             });
         e.preventDefault();
     });
-    var $btnStore = $('#btnStore');
-    var $previewDiv = $('#previewDiv');
+
     $btnStore.on('click', function(e){
         $Gen.show();
         $.post('/lots/search',{catNo: $inputCatNo.val(), lotNo: $inputLotNo.val(), mfgDate: $inputMFGDate.val()
@@ -149,29 +191,12 @@ $(document).ready(function() {
             });
         e.preventDefault();
     });
-//    setInterval(function(){
-//        app.setPrevStatus(app.getStatus());
-//
-//        $.get('/status', function(data){
-//            if(data.status != "on"){
-//                $srvOffAlert.show();
-//                app.srvStatus = data.status;
-//                setTimeout(function(){
-//                    $srvOffAlert.hide();
-//                },3000);
-//            }else{
-//                if(app.prevSrvStatus == "off"){
-//                    $srvBackAlert.show();
-//                    setTimeout(function(){
-//                        $srvBackAlert.hide();
-//                    },3000);
-//                }
-//                app.srvStatus = data.status;
-//            }
-//        });
-//    },5000);
 
 
+    $window.scroll(function() {
+//        var position = $window.scrollTop();
+        $divAlert.css({"top": ( $divAlert.height()) + "px"});
+    });
 });
 
 
